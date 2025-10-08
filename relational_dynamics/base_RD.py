361a3abdb97d6d3941d029a2eebaa97128075fad
Chrisantus Eze <chrisantus.eze@example>
2025-10-08 11:46:15 -0500

    made some changes

--- relational_dynamics/base_RD.py
+++ relational_dynamics/base_RD.py
@@ -44,6 +44,8 @@ from relational_dynamics.config.base_config import BaseConfig
 from itertools import permutations
 from torch.utils.tensorboard import SummaryWriter
 
+import model_utils
+
 
 class RelationalDynamics(object):
     """Main class to train and run relational dynamics models.
@@ -307,26 +309,12 @@ class RelationalDynamics(object):
 
         return current_latent, discrete_action, continuous_action, current_action_continuous, current_action, skill_label
 
-    def _build_masks(self, x_tensor_dict):
-        """Construct commonly used masks for training loss computation.
-
-        Returns a dict with masks keyed by name.
-        """
-        masks = {}
-        masks['relational_mask'] = (x_tensor_dict['batch_all_obj_pair_relation']==-1)
-        masks['env_mask'] = (x_tensor_dict['batch_env_identity']==-1)
-        masks['object_level_mask'] = masks['env_mask'][:, :, :, [0]]
-        masks['latent_space_mask'] = masks['object_level_mask'].repeat(1,1,1,256)
-        masks['graspable_mask'] = (x_tensor_dict['batch_grasp_identity']==-1)
-        masks['position_mask'] = (x_tensor_dict['batch_6DOF_pose']==-1)
-        return masks
-
     def _compute_losses(self, current_output_classifier_list, current_env_identity_list, current_grasp_identity_list, pred_latent, current_latent_list, predicted_pose, predicted_predicates, predicted_env, predicted_feasibility, x_tensor_dict):
         """Compute and return the total loss using internal loss functions and masks.
 
         This helper mirrors the previous inline loss computation.
         """
-        masks = self._build_masks(x_tensor_dict)
+        masks = model_utils.build_masks(x_tensor_dict)
         total_loss = 0
         for i in range(len(current_output_classifier_list)):
             total_loss += self.bce_loss(current_output_classifier_list[i][~masks['relational_mask'][i]], x_tensor_dict['batch_all_obj_pair_relation'][i][~masks['relational_mask'][i]])
@@ -360,108 +348,6 @@ class RelationalDynamics(object):
         self.emb_model.load_state_dict(cp_models['emb_model'])
         self.classif_model.load_state_dict(cp_models['classif_model'])
         self.classif_model_decoder.load_state_dict(cp_models['classif_model_decoder'])
-
-    def process_data(self, batch_data, push_3_steps = False): 
-        '''Process raw batch data and collect relevant objects in a dict.
-
-        Input: batch_data is a list of per-scene dicts produced by the DataLoader.
-        Output: x_dict contains tensors organized for training: stacked along
-        batch dimension (and sequence/time dimension where applicable).
-        Values set to -1 in the data indicate masked / missing entries and are
-        respected later during loss computation.
-        '''
-        args = self.config.args
-        x_dict = dict()
-
-        
-        # assert(len(batch_data) == 1)
-
-        x_dict['batch_num_objects'] = []
-        x_dict['batch_action'] = []
-        x_dict['batch_all_obj_pair_relation'] = []
-        x_dict['batch_one_hot_encoding'] = []
-        x_dict['batch_skill_label'] = []
-        x_dict['batch_voxel_list_single'] = []
-        x_dict['batch_env_identity'] = []
-        x_dict['batch_grasp_identity'] = []
-        x_dict['batch_6DOF_pose'] = []
-        x_dict['support_suface_id'] = []
-        x_dict['buffer_tensor_0'] = []
-
-        # Stack items from every element in batch_data into lists, then convert
-        # to tensors (or keep as lists where appropriate). The DataLoader
-        # already formats keys like 'all_object_pair_voxels_single' so we keep
-        # the same naming and shapes expected by downstream code.
-        for b, data in enumerate(batch_data):
-            x_dict['batch_num_objects'].append(data['num_objects'])
-            x_dict['batch_action'].append(data['action'])
-            x_dict['batch_all_obj_pair_relation'].append(data['relation'])
-            x_dict['batch_one_hot_encoding'].append(data['one_hot_encoding'])
-            x_dict['batch_skill_label'].append(data['all_action_label'])
-            x_dict['batch_voxel_list_single'].append(data['all_object_pair_voxels_single'])
-            x_dict['batch_env_identity'].append(data['env_identity'])
-            x_dict['batch_grasp_identity'].append(data['all_gt_grapable_list'])
-            x_dict['batch_6DOF_pose'].append(data['all_6DOF_pose_fast'])
-            x_dict['batch_edge_attr'] = data['edge_attr']
-            x_dict['batch_obj_boundary'] = data['all_obj_boundaty']
-            x_dict['batch_position'] = data['position']
-            x_dict['batch_quaternian'] = data['quaternian']
-            x_dict['batch_extents'] = data['extents']
-            x_dict['batch_all_hidden_tensor'] = data['all_hidden_tensor']
-            x_dict['new_latent'] = data['new_latent']
-            x_dict['support_suface_id'].append(data['support_suface_id'])
-            x_dict['buffer_tensor_0'].append(data['buffer_tensor_0'])
-            x_dict['batch_all_hidden_label'] = data['all_hidden_label']
-
-        # Convert lists to stacked tensors for efficient batch processing
-        x_dict['batch_env_identity'] = torch.squeeze(torch.stack(x_dict['batch_env_identity']), 1)
-        x_dict['batch_all_obj_pair_relation'] = torch.squeeze(torch.stack(x_dict['batch_all_obj_pair_relation']), 1)
-        x_dict['batch_voxel_list_single'] = torch.squeeze(torch.stack(x_dict['batch_voxel_list_single']), 1)
-        x_dict['batch_action'] = torch.squeeze(torch.stack(x_dict['batch_action']), 1)
-        x_dict['batch_skill_label'] = np.squeeze(np.stack(x_dict['batch_skill_label']), 1)
-        x_dict['batch_one_hot_encoding'] = torch.squeeze(torch.stack(x_dict['batch_one_hot_encoding']), 1)
-        x_dict['batch_grasp_identity'] = torch.squeeze(torch.stack(x_dict['batch_grasp_identity']), 1)
-        x_dict['batch_6DOF_pose'] = torch.squeeze(torch.stack(x_dict['batch_6DOF_pose']), 1)
-
-        return x_dict
-
-    def process_data_plan(self, batch_data, push_3_steps = False): 
-        '''Process data for planning (assumes batch size == 1).
-
-        This is similar to `process_data` but keeps a single scene and does not
-        stack across a batch dimension. The planner expects single-scene input.
-        '''
-        args = self.config.args
-        x_dict = dict()
-
-        # Planner operates on single-scene inputs
-        assert(len(batch_data) == 1)
-
-        for b, data in enumerate(batch_data):
-            x_dict['batch_num_objects'] = data['num_objects']
-            x_dict['batch_action'] = data['action']
-            x_dict['batch_all_obj_pair_relation'] = data['relation']
-            x_dict['batch_one_hot_encoding'] = data['one_hot_encoding']
-            x_dict['batch_edge_attr'] = data['edge_attr']
-            x_dict['batch_skill_label'] = data['all_action_label']
-            x_dict['batch_voxel_list_single'] = data['all_object_pair_voxels_single']
-            x_dict['batch_env_identity'] = data['env_identity']
-            x_dict['batch_grasp_identity'] = data['all_gt_grapable_list']
-            x_dict['batch_6DOF_pose'] = data['all_6DOF_pose_fast']
-            x_dict['batch_obj_boundary'] = data['all_obj_boundaty']
-
-            x_dict['batch_position'] = data['position']
-            x_dict['batch_quaternian'] = data['quaternian']
-            x_dict['batch_extents'] = data['extents']
-            x_dict['batch_all_hidden_tensor'] = data['all_hidden_tensor']
-            x_dict['new_latent'] = data['new_latent']
-
-            x_dict['support_suface_id'] = data['support_suface_id']
-            x_dict['buffer_tensor_0'] = data['buffer_tensor_0']
-
-            x_dict['batch_all_hidden_label'] = data['all_hidden_label']
-
-        return x_dict
 
     def training(self,
                 x_tensor_dict,
@@ -526,7 +412,7 @@ class RelationalDynamics(object):
          current_action, skill_label) = self._get_action_embeddings(x_tensor_dict, seq, current_latent_list)
 
 
-        # Concatenate node latents with action features to form graph node inputs
+        # Concatenate node latents with action features to form graph node inputs - @Chris: This is where we chain the inputs to the dynamics model
         graph_node_action = torch.cat((current_latent, current_action_continuous, current_action), axis = 1)
 
         # Run skill-conditioned dynamics per-batch element and collect next-latent
@@ -575,26 +461,6 @@ class RelationalDynamics(object):
             self.opt_classif_decoder.step()
     
         return batch_result_dict
-
-    def collision_check_2D(self, bounding_box_1, bounding_box_2):
-        bbox1_left = bounding_box_1[0][0]
-        bbox1_right = bounding_box_1[2][0]
-        bbox1_top = bounding_box_1[0][1]
-        bbox1_bottom = bounding_box_1[1][1]
-
-        bbox2_left = bounding_box_2[0][0]
-        bbox2_right = bounding_box_2[2][0]
-        bbox2_top = bounding_box_2[0][1]
-        bbox2_bottom = bounding_box_2[1][1]
-
-        # standard axis-aligned bbox overlap check
-        if (bbox1_left < bbox2_right and
-            bbox1_right > bbox2_left and
-            bbox1_top < bbox2_bottom and
-            bbox1_bottom > bbox2_top):
-            return True
-        else:
-            return False
     
     def planner(self):
         # This helper performs the sampling-based planning and simulation loop
@@ -833,7 +699,7 @@ class RelationalDynamics(object):
                                 
                             for each_collision_i in range(len(self.collision_boundary) - 1):
                                 for each_collision_j in range(each_collision_i + 1, len(self.collision_boundary)):
-                                    if self.collision_check_2D(self.collision_boundary[each_collision_i], self.collision_boundary[each_collision_j]) or self.collision_check_2D(self.collision_boundary[each_collision_j], self.collision_boundary[each_collision_i]):
+                                    if model_utils.collision_check_2D(self.collision_boundary[each_collision_i], self.collision_boundary[each_collision_j]) or model_utils.collision_check_2D(self.collision_boundary[each_collision_j], self.collision_boundary[each_collision_i]):
                                         feasibility_leap = 0
 
                             if feasibility_leap == 1: ## ground 
@@ -852,7 +718,7 @@ class RelationalDynamics(object):
                                             
                                 for each_collision_i in range(len(self.collision_boundary) - 1):
                                     for each_collision_j in range(each_collision_i + 1, len(self.collision_boundary)):
-                                        if self.collision_check_2D(self.collision_boundary[each_collision_i], self.collision_boundary[each_collision_j]) or self.collision_check_2D(self.collision_boundary[each_collision_j], self.collision_boundary[each_collision_i]):
+                                        if model_utils.collision_check_2D(self.collision_boundary[each_collision_i], self.collision_boundary[each_collision_j]) or model_utils.collision_check_2D(self.collision_boundary[each_collision_j], self.collision_boundary[each_collision_i]):
                                             feasibility_leap = 0
                     
                     
@@ -1015,11 +881,11 @@ class RelationalDynamics(object):
 
 
                     if train:
-                        x_tensor_dict = self.process_data(batch_data)
-                        x_tensor_dict_next = self.process_data(batch_data_next)
+                        x_tensor_dict = model_utils.process_data(self.config, batch_data)
+                        x_tensor_dict_next = model_utils.process_data(self.config, batch_data_next)
                     else:
-                        x_tensor_dict = self.process_data_plan(batch_data)
-                        x_tensor_dict_next = self.process_data_plan(batch_data_next)
+                        x_tensor_dict = model_utils.process_data_plan(self.config, batch_data)
+                        x_tensor_dict_next = model_utils.process_data_plan(self.config, batch_data_next)
 
                     if train:
                         batch_result_dict = self.training(
